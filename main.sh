9aae96cb6c8c04152b8252b7434ea3b6ef6eb031
josephexequiel <52370507+josephexequiel@users.noreply.github.com>
2021-04-07 21:02:52 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,7 @@
 LOG_FILE="$1";
 
-cat "$LOG_FILE" | awk '{print $4}' | awk -F ':' '{print $1}' | tr -d '[' | sort | uniq -c | awk '{print $1=$1,$2}' | sort -r
-cat "$LOG_FILE" | awk '{print $1=$1}' | sort | uniq -c | awk '{print $1=$1,$2}' | sort -r
 ip_list=$(cat "$LOG_FILE" | awk '{print $1=$1}' | sort | uniq -c | awk '{print $1=$1,$2}' | sort -r);
 date_list=$(cat "$LOG_FILE" | awk '{print $4}' | awk -F ':' '{print $1}' | tr -d '[' | sort | uniq -c | awk '{print $1=$1,$2}' | sort -r);
+
 echo "$ip_list";
 echo "$date_list";
